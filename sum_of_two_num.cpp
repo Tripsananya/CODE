2420d262a5716abf5c6144b7652be35adff421c8
Tripsananya <98031173+Tripsananya@users.noreply.github.com>
2022-01-22 19:48:11 +0530

    Add files via upload

---
+++ sum_of_two_num.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    a=30;
+    b=70;
+    sum=a+b;
+    cout<<" SUM OF "<<a<<" and  "<<b<< " = "<<sum;
+    return 0;
+} 
