ac7daa52fa92f576e748cf2ec7a7c1fef930a225
Tripsananya <98031173+Tripsananya@users.noreply.github.com>
2022-01-25 11:57:52 +0530

    Add files via upload

---
+++ area_circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#define PI 3.141
+int main()
+{
+int r;
+float area;
+cout<<"Enter radius :";
+cin>>r;
+area= PI * r *r;
+cout<<"Area= "<<area;
+return 0;
+}
