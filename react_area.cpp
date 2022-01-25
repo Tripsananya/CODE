a9573fe12b374ce74dd349841a4390ef7633f72c
Tripsananya <98031173+Tripsananya@users.noreply.github.com>
2022-01-25 11:56:13 +0530

    Add files via upload

---
+++ react_area.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int s1,s2;
+    
+    cout<<"Enter the side 1:";
+    cin>>s1;
+    cout<<"Enter the side 2:";
+    cin>>s2;
+    int area;
+    area = s1*s2;
+    cout<<"The area is "<<area;
+    return 0;
+
+}
