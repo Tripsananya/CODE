65ea8637853518e90b03d874c4475e93b9680626
Tripsananya <98031173+Tripsananya@users.noreply.github.com>
2022-02-14 14:30:37 +0530

    Add files via upload

---
+++ star.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 9; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+int i,j;
+for (i=9;i>=1;i--)
+{
+for(j=1;j<=i;j++)
+{
+cout<<"*";
+}
+cout<<endl;
+}
+return 0;
+}
+
+*/
